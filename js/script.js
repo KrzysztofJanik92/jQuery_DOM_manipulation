7cb8150ad036d80c754fa94c333a78e05b8572ab
KrzysztofJanik92 <krzysztof.janik@vp.pl>
2017-03-09 17:32:54 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,64 +1,57 @@
-var slideMenu = function() {
-  /* Push the body and the nav over by 285px over */
-  $('#icon-menu').click(function() {
-    $('#menu').animate({
-      left: "0px"
-    }, 400);
+var main = function(){
+/* Push the body and the nav over by 285px over */
+   $('#icon-menu').click(function() {
+      $('#menu').animate({
+         left: "0px"
+      }, 400);
 
-    $('body').animate({
-      left: "200px"
-    }, 400);
-    
-     $("#m2").hide(400);  
-     
-  });
+      $('body').animate({
+         left: "200px"
+      }, 400);
 
-  /* Then push them back */
-  $('#icon-close').click(function() {
-    $('#menu').animate({
-      left: "-200px"
-    }, 400);
+      $("#m2").hide(400);  
 
-    $('body').animate({
-      left: "0px"
-    }, 400);
-      
-    $("#m2").show(400);
-  });
-};
+   });
+
+/* Then push them back */
+   $('#icon-close').click(function() {
+      $('#menu').animate({
+         left: "-200px"
+      }, 400);
+
+      $('body').animate({
+         left: "0px"
+      }, 400);
+
+      $("#m2").show(400);
+   });
 
 
-$(document).ready(slideMenu);
+   $('.btn').click(function() {
+      var post = $('.textBox').val();
+      $('<li>').text(post).prependTo('.posts');
+      $('.textBox').val('');
+      $('.label').text('140');
+      $('.btn').addClass('disabled');
+   });
 
-var postBox = function() {
-    $('.btn').click(function() {
-        var post = $('.textBox').val();
-        $('<li>').text(post).prependTo('.posts');
-        $('.textBox').val('');
-        $('.label').text('140');
-        $('.btn').addClass('disabled');
-        });
-        
-    $('.textBox').keyup(function() {
+   $('.textBox').keyup(function() {
       var postLength = $(this).val().length;
       var charactersLeft = 140 - postLength;
-    $('.label').text(charactersLeft);
-    
-    if(charactersLeft < 0) {
-      $('.btn').addClass('disabled'); 
-      }
-      else if(charactersLeft == 140) {
-         $('.btn').addClass('disabled');
+      $('.label').text(charactersLeft);
+
+      if(charactersLeft < 0 || charactersLeft == 140) {
+         $('.btn').addClass('disabled'); 
       }
       else {
          $('.btn').removeClass('disabled');
       }
-    });
+   });
 
-    $('.btn').addClass('disabled');
-    
+$('.btn').addClass('disabled');
+
 }
-    
-$(document).ready(postBox);
+
+$(document).ready(main);
 
 
